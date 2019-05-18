ccce58ac10e432795941a7168a0170bbe2a081b0
LAPTOP-KUMTRKSA\willi <willoswe99@gmail.com>
2019-05-18 13:59:39 +1000

    Imported AI scripts that we have so far.

---
+++ ProjectPhoenix/Assets/Scripts/AIMovement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIMovement : MonoBehaviour
+{
+    private GameObject room;
+    public GameObject[] rooms;
+    private Vector3 AiDestination;
+    private NavMeshAgent agent;
+    [SerializeField]
+    private bool patrollingRoom;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        rooms = GameObject.FindGameObjectsWithTag("Room");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Debug.Log(AiDestination);
+
+        if (!patrollingRoom)
+        {
+            room = rooms[UnityEngine.Random.Range(0, rooms.Length)];
+            AiDestination = GetRandomPosInsideBox(room.transform.position, room.GetComponent<Collider>().bounds.size);
+
+            if (Physics.CheckSphere(AiDestination, 0.5f))
+            {
+                agent.SetDestination(AiDestination);
+                patrollingRoom = true;
+            }
+
+        }
+        if (!agent.pathPending)
+        {
+            if (agent.remainingDistance <= 0.5f)
+            {
+                Invoke("ResetRoomPatrol", 2);
+            }
+        }
+
+
+    }
+    void ResetRoomPatrol()
+    {
+        CancelInvoke("ResetRoomPatrol");
+        patrollingRoom = false;
+    }
+
+
+    Vector3 GetRandomPosInsideBox(Vector3 center, Vector3 size)
+    {
+        Vector3 rndP = new Vector3(
+            size.x * (UnityEngine.Random.value - .5f),
+            0,
+            size.z * (UnityEngine.Random.value - .5f));
+        return center + rndP;
+
+    }
+}
---
+++ ProjectPhoenix/Assets/Scripts/DrawRoomTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// place this script on a empty Gameobject
+
+[RequireComponent(typeof(BoxCollider))]
+public class DrawRoomTest : MonoBehaviour
+{
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(0f, 1f, 0f);
+        Gizmos.DrawWireCube(GetComponent<BoxCollider>().transform.localPosition, GetComponent<BoxCollider>().transform.localScale);
+
+        Gizmos.color = new Color(0f, 1f, 0f, 0.3f);
+        Gizmos.DrawCube(GetComponent<BoxCollider>().transform.localPosition, GetComponent<BoxCollider>().transform.localScale);
+    }
+}
