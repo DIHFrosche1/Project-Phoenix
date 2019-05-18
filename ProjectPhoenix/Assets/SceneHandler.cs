1ffc7db2e7967d5c9813afa140479cc4ac9d8a6b
LAPTOP-KUMTRKSA\willi <willoswe99@gmail.com>
2019-05-18 16:23:48 +1000

    Added Extra scenes, and created a script to change scene with buttons

---
+++ ProjectPhoenix/Assets/SceneHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneHandler : MonoBehaviour
+{
+
+    public string currentScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void LoadScene(string scene)
+    {
+
+
+        currentScene = scene;
+        SceneManager.LoadScene(currentScene);
+
+
+    }
+}
