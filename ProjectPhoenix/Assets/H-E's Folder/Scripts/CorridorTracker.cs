59a8ab305dd036d88c1732006eb39871d9df1a35
H-E <hemangameing@gmail.com>
2019-05-19 10:47:31 +0200

    Added scripts

---
+++ ProjectPhoenix/Assets/H-E's Folder/Scripts/CorridorTracker.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// This script will keep track of how many times the player has walked past a certain point within the last couple of minutes. 
+/// If the player walks past a certain point more than X amount of times within the time frame it will alert the AI to Hunt there or Guard there.
+/// Place this script on every key point of the map, set a time limit (so that it doesn't keep track of a place you haven't been in in a very long time) and set an "alert" cap (how many times you have to pass by before it alerts the AI)
+/// The Alert cap can be 2 different variables so that it adds a random element to if the AI is alerted or not. The min value is the amount of times the player has to pass by the point before there is a chance to alert the AI and the max value is the amount of times the player has to pass by the point for there to be a 100% chance to alert the AI. 
+/// There should RARELY be a time where the player raches the max value.
+/// 
+/// </summary>
+
+public class CorridorTracker : MonoBehaviour
+{
+
+    public int timesPassed;
+    public int timesPassedMinCap;
+    public int timesPassedMaxCap;
+    public float timeCap;
+    public int chanceOfAlertingOnMin; // Out of 100, so 20 is 20%, 50 if 50% and so on.
+    //public int chanceOfAlertingOnMax;
+
+    private float timer;
+    private int rand;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= timeCap)
+        {
+            timesPassed = 0;
+            timer = 0;
+        }
+
+        if (timesPassed >= timesPassedMinCap)
+        {
+            if (timesPassed >= timesPassedMaxCap)
+            {
+                //Alert the AI
+            }
+            else
+            {
+                //Random chance of alerting the AI
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        timesPassed++;
+        timer = 0;
+        rand = Random.Range(0, 100);
+    }
+}
--- ProjectPhoenix/Assets/H-E's Folder/Scripts/MacroAI.cs
+++ ProjectPhoenix/Assets/H-E's Folder/Scripts/MacroAI.cs
@@ -2,36 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This code will decide what the different states will do for the AI. 
+/// 
+/// Wandering = walking around between different rooms.
+/// Hunting = walking around in a specific room (often to looking for the player the player).
+/// Guarding = walking around in a specific area of a room or staying still and watching a specific area.
+/// Investigating = going to a place where it has been alerted that the player has walked by X amount of times.
+/// 
+/// Wandering is the starting state and the state that is the default if the player is not close or the Investigating requierments have been met.
+/// Hunting is when the player is close or when the Investigating state has been fullfilled and the room in which the AI should investigate is the same room as the room he is in. Should have a time limit on how long the AI can "hunt"
+/// Guaring is like hunting but a smaller than a room area. Should also have a time limit.
+/// Investigating can only be activated via the CorridorTracker script.
+///  
+/// </summary>
+
 public class MacroAI : MonoBehaviour
 {
 
     public enum States
     {
-        //InRoom1,
-        //InRoom2,
-        //WalkingToRoom1,
-        //WalkingToRoom2,
         Wandering,
         Hunting,
         Guarding,
+        Investigating,
     }
 
-    public GameObject currentZone;
+    public States currentState;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentState = States.Wandering;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (currentState == States.Wandering)
+        {
+            //Wander between different rooms.
+        }
+        else if (currentState == States.Hunting)
+        {
+            //Pick random points in the room to walk to.
+        }
+        else if (currentState == States.Guarding)
+        {
+            //Pick random points within an area of the guarding point.
+        }
+        else if (currentState == States.Investigating)
+        {
+            //Go to the specified room and hunt in there.
+        }
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        currentZone = other.gameObject;
-    }
+
+    
 }
