08929f931cccdbb0f27e020d6d0bb36966129829
H-E <hemangameing@gmail.com>
2019-05-19 08:59:23 +0200

    H-E's folder added

---
+++ ProjectPhoenix/Assets/H-E's Folder/Scripts/MacroAI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MacroAI : MonoBehaviour
+{
+
+    public enum States
+    {
+        //InRoom1,
+        //InRoom2,
+        //WalkingToRoom1,
+        //WalkingToRoom2,
+        Wandering,
+        Hunting,
+        Guarding,
+    }
+
+    public GameObject currentZone;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        currentZone = other.gameObject;
+    }
+}
